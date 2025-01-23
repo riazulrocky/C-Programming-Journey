5078567fe2fc9222f2ca1645c6293e9b923b79cd
Riazul Hasan Rocky <134952141+riazulrocky@users.noreply.github.com>
2025-01-23 22:38:51 +0600

    Add files via upload

---
+++ Swap Two Varriable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter value of a : ");
+    scanf("%d", &a);
+    printf("Enter value of b : ");
+    scanf("%d", &b);
+
+    a = a^b;
+    b = a^b;
+    a = a^b;
+
+    printf("\n - After Swapping \n");
+    printf("Value of a : %d \n", a);
+    printf("Value of b : %d", b);
+
+    return 0;
+}
