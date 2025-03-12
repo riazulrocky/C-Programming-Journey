c3f38065b3cdb87ee378307bcf4ef9b70c749d0e
Riazul Hasan Rocky <134952141+riazulrocky@users.noreply.github.com>
2025-03-13 04:50:23 +0600

    Add files via upload

---
+++ Calculate area of square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int side;
+
+    printf("Enter side : ");
+    scanf("%d", &side);
+
+    printf("Area of square is : %d\n", side*side);
+
+    return 0;
+}
